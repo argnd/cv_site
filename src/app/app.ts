459b738a38e29e7da3c5d85faeb71c06031cf4ef
armel <gandour37@gmail.com>
2026-08-23 08:20:14 +0200

    init

---
+++ src/app/app.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  styleUrl: './app.css',
+  templateUrl: './app.html',
+})
+export class App {
+  protected readonly title = 'CV Site';
+}
